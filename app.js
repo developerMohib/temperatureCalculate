4eeee3b70acfdbe9e7b6dcaac91ce47fa011effb
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-09 00:21:36 +0600

    Temperature Calculator

---
+++ app.js
@@ -0,0 +1,14 @@
+// now we make a celcius fahrenhight kelvin calculator
+
+// celcius to fahrenhight 
+
+let celci = parseInt(45);
+let fahren = (celci * 9/5) + 32 ;
+
+console.log( celci + ` Celcius temperature = `+ fahren + ` Fahrenhight. `);
+
+// Fahrenhight to celsius 
+
+let Fahrenhight =parseInt( 113);
+let celsius = ((Fahrenhight - 32)* 5 )/9 ;
+console.log( Fahrenhight + ` Fahrenhight temperature = `+ celsius + ` Celsius. `)
